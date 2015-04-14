c21b7e56857aa312e5d8fa413ea670869b2abe91
agustinlongato <aguslongato@hotmail.com>
2015-04-14 00:47:03 -0300

    TP 1
    archivo cpp y salida txt

---
+++ main.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+using namespace std;
+int main () {
+	string a = "Carlos tiene " , b = " años" , c = " y pablo " ;
+	int d = 20 , e = 25 ;
+	char coma = ',' , punto = '.' ;
+	cout << a << d << b << coma << c << e << punto << endl ;
+}
+
